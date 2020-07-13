c0913fe9cb230a4e5fbfef46ccfc0b2ece873312
TattiTV <58032454+TattiTV@users.noreply.github.com>
2020-07-13 22:04:26 +0300

    updated header links urls (create 4 new Pages!)
    Now code refers to new sites so
     in wordpres dashboard -> Pages -> add new
    - create four sites: Kuvagalleria, Liity Jäseneksi, Ota yhteyttä, Tapahtumat and Tietoa meistä.

--- header.php
+++ header.php
@@ -15,10 +15,10 @@
         <nav class="main-navigation">
           <ul>
             <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 16) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/tietoa-meista') ?>">Tietoa meistä</a></li>
-            <li <?php if (get_post_type() == 'program') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('program') ?>">Liity jäseneksi</a></li>
-            <li <?php if (get_post_type() == 'event' OR is_page('past-events')) echo 'class="current-menu-item"';  ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Tapahtumat</a></li>
-            <li <?php if (get_post_type() == 'campus') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('campus'); ?>">Kuvagalleria</a></li>
-            <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog'); ?>">Ota yhteyttä!</a></li>
+            <li <?php if (get_post_type() == 'program') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/liity-jaseneksi') ?>">Liity jäseneksi</a></li>
+            <li <?php if (get_post_type() == 'event' OR is_page('past-events')) echo 'class="current-menu-item"';  ?>><a href="<?php echo site_url('/tapahtumat') ?>">Tapahtumat</a></li>
+            <li <?php if (get_post_type() == 'campus') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/kuvagalleria') ?>">Kuvagalleria</a></li>
+            <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/ota-yhteytta'); ?>">Ota yhteyttä!</a></li>
           </ul>
         </nav>
         
