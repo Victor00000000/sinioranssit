8feb0fec82a9e5f6ff31a4913fefcf7649cad4c1
TattiTV <58032454+TattiTV@users.noreply.github.com>
2020-07-17 11:33:42 +0300

    Custom image sizes added / changed

--- functions.php
+++ functions.php
@@ -2,14 +2,16 @@
 
 function pageBanner($args = NULL) {
   
-  //get title from page info
+
   if (!$args['title']) {
     $args['title'] = get_the_title();
   }
+ 
 
   if (!$args['subtitle']) {
     $args['subtitle'] = get_field('page_banner_subtitle');
   }
+
 
   if (!$args['photo']) {
     if (get_field('page_banner_background_image')) {
@@ -51,9 +53,9 @@ add_action('wp_enqueue_scripts', 'university_files');
 function university_features() {
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
-  add_image_size('professorLandscape', 400, 260, true);
-  add_image_size('professorPortrait', 480, 650, true);
-  add_image_size('pageBanner', 1500, 350, true);
+  add_image_size('CustomImageSizeS', 400, 260, true); /* Cropping blugin:Image S Size */
+  add_image_size('CustomImageSizeM', 480, 650, true); /* Cropping blugin: Image M size */
+  add_image_size('pageBanner', 1500, 350, true); /* Cropping blugin: page Banner image size */
 }
 
 add_action('after_setup_theme', 'university_features');
